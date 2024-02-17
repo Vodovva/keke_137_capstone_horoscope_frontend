9ce57e05b9924718b7096496d2c9f48df74ca0a1
Vodovva <valerievodovnik@gmail.com>
2024-02-16 19:59:13 -0600

    carousel added with daily images

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,61 +1,14 @@
-import { useState, useEffect, ChangeEvent } from 'react';
-import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import Carousel from '../components/Carousel';
 
-
-
-type HomeProps = {
-    isLoggedIn:boolean,
-    currentUser: UserType|null,
-    flashMessage: (message:string, category:CategoryType) => void
-}
-
-
-export default function Home({ isLoggedIn, currentUser, flashMessage }: HomeProps) {
-    const [user, setUser] = useState<UserType[]>([]);
-    const [newUser, setNewUser] = useState<PostFormDataType>({title: '', body: ''})
-    const [displayForm, setDisplayForm] = useState(false);
-    const [fetchSignData, setFetchSignData] = useState(true)
-
-    useEffect( () => {
-        async function fetchData(){
-            const response = await getAllSign();
-            console.log(response);
-            if (response.data){
-                let posts = response.data;
-                Sign.sort( (a, b) => (new Date(a.dateCreated) > new Date(b.dateCreated)) ? -1 : 1)
-                setSign(sign)
-            }
-        }
-
-        fetchData();
-    }, [fetchSignData] )
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        // console.log(event.target.value, event.target.name);
-        setNewSign({...newPost, [event.target.name]: event.target.value})
-    }
-
-    const handleFormSubmit = async (event: React.FormEvent) => {
-        event.preventDefault();
-        
-        const token = localStorage.getItem('token') || '';
-        const response = await createSign(token, newUser);
-        if (response.error){
-            flashMessage(response.error, 'danger');
-        } else {
-            flashMessage(`${response.data?.title} has been created`, 'success');
-            setNewSign({title:'', body:''})
-            setDisplayForm(false);
-            setFetchSignData(!fetchSignData);
-        }
-
-    }
-
+export default function Home() {
 
 
     return (
         <Container fluid>
+            <Carousel/>
+            <br/>
             <h2 className="text-center">Astrology Signs</h2> 
             <Row xs={1} md={4} style={{paddingTop: '2rem', paddingBottom: '2rem'}} className="g-4">
                 {Array.from({ length: 4 }).map((_, idx) => (
